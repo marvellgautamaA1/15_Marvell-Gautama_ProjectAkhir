09a19d80a18ce886391437065d0c551aa89105f8
marvellgautamaA1 <2020070550@student.pppkpetra.sch.id>
2025-11-14 20:41:37 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "mysql", "proyek akhir");
-//echo("koneksi berhasil");
-//cek koneksi
-if (mysqli_connect_error()) {
-    echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-
-?>
